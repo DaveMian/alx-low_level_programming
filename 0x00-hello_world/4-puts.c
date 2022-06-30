b520d7a813af86c4476379eea03463367bc1f871
Dawit Wubshet Gebremedhin <davemain32@gmail.com>
2022-06-29 23:44:57 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int main(void)
-{
-	puts("\"
-			Programming is like building a multilingual puzzle");
-	return (0);
-}
+ /** 
+  *
+  *   * main - main block 
+  *
+  *     * Return: 0 
+  *
+  *       */ 
 
+ int main(void) 
+
+	 { 
+
+		  puts("\"Programming is like building a multilingual puzzle"); 
+
+		   return (0); 
+
+		    }
