af1668329f847871568f3d9c365ed8756ecaeef8
Dawit Wubshet Gebremedhin <davemain32@gmail.com>
2022-07-01 07:14:20 -0700

    last supper

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 /**
-<<<<<<< HEAD
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
- */
-=======
 * main - prints all possible combinations of two two-digit numbers
-* Return: 0
+* Return: Always 0 (Success)
 */
->>>>>>> 85d71601f07dc7d281687fe027049f686f004d77
 int main(void)
 {
 int i, j;
