8ed8af56f2aad2561d9890d3f0fc53a21d2c73b6
Dawit Wubshet Gebremedhin <davemain32@gmail.com>
2022-07-01 01:27:13 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet is lower case,
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch));
+	}
+	putchar('\n');
+	return (0);
+}
