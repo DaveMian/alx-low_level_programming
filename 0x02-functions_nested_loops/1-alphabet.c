cb0f5ca85d43d1ed54b1a9e25778b8626b8ed9a2
Dawit Wubshet Gebremedhin <52971221+DaveMian@users.noreply.github.com>
2022-07-05 14:22:46 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
- /**
-  * print_alphabet - print alphabet 
-  * Return: void 
-  */
+/**
+ * print_alphabet - print alphabet
+ * Return: void
+ */
 void print_alphabet(void) 
 {
 char letter = 'a';	    
@@ -10,6 +10,6 @@ while (letter <= 'z')
 {
 _putchar(letter);
 letter++;
-} 
+}
 _putchar('\n');
 }
