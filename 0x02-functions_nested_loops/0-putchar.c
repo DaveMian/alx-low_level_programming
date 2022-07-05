c55ba526774b55c694ea61e804c1b238b9821a6e
Dawit Wubshet Gebremedhin <davemain32@gmail.com>
2022-07-04 22:44:44 -0700

    put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdiod.io>
+/**
+ * main - _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+char text[9] = "_putchar";
+int i 0;
+for (i = o, i< 0; i++)
+{
+_putschar(text[i]);
+}
+_putschar('\n');
+return (0);
+}
