5d28840ce7940222dd447268c9fd9a5daff17951
Dawit Wubshet Gebremedhin <davemain32@gmail.com>
2022-07-05 04:29:24 -0700

    10 x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,36 @@
+#include "main.h"
+ /**
+  * print_alphabet_x10 - print alphabet 10 times
+  * Return: void
+  */
+void print_alphabet_x10(voidint round = 0; 
+
+			           char letter = 'a'; 
+
+				     
+
+				            while (round < 10) 
+
+						             { 
+
+								                      letter = 'a'; 
+
+										                       while (letter <= 'z') 
+
+													                        { 
+
+																	                         _putchar(letter); 
+
+																				                          letter++; 
+
+																							                   } 
+
+												                        _putchar('\n'); 
+
+															  
+
+															                 round++; 
+
+																	          } 
+
+					     }
